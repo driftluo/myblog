cabc82c6116ea8f2723f62609082c6175ecbf0b0
漂流 <driftluo@users.noreply.github.com>
2017-12-11 12:55:18 +0800

    update lua script
    `rpush` to `lpush` bug

--- lua/visitor_log.lua
+++ lua/visitor_log.lua
@@ -5,7 +5,7 @@ local len = redis.call("llen", redis_key);
 redis.call("lpush", redis_key, ip)
 
 if len > 100 then
-    redis.call("ltrim", redis_key, len - 99, len)
+    redis.call("ltrim", redis_key, 0, 100 - 1)
 end
 
 return true
